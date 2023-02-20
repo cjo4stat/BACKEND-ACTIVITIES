275ee99fc9f563a8f902db42375fb2f23811ea6c
cjo4stat <cjo4stat@gmail.com>
2023-02-20 12:33:40 +0100

    I hope it goes well

---
+++ classwork.js
@@ -0,0 +1,193 @@
+// let userName = prompt("What is your name? ");
+
+// let trenchesType =  prompt("Where do you stay on campus? (Mariere, Eni-njoku, Jaja, Biobaku ) ");
+
+
+// if (trenchesType === "Mariere") {
+
+//     console.log("Hey anonymous, so sorry about your predicament")
+
+//     for (let i = 0 ; i < 20; i++ ) {
+
+//         clg("Mariere");'
+
+//     }
+
+// } else if (trenchesType -=== "Biobaku") {
+
+//  console.log(stuff about biobaku)
+
+//     // Print biobaku 20 times
+
+// }
+
+// for (let i = 0; i < 20; i++) {
+//     alert (Ikorodu);
+// }
+
+// let userName = prompt("WHAT IS YOUR NAME");
+// let cityTypes = prompt("WHERE DO YOU LIVE(Ikeja, Lekki, Ikorodu, Epe)");
+// let m = "Ikeja";
+// let n = "Lekki";
+// let x = "Ikorodu";
+// let y = "Epe";
+
+// let A = "You are doing great,";
+// let B = "So sorry I did not make it up due to Bad road,";
+// let C = "espect my arival soon"
+
+// if ("cityTypes === x") {
+    
+//     let results = B.repeat(20);
+//     alert(results);
+
+//   } else if ("cityTypes === Lekki") {
+//     alert(A);
+//     let result = A.repeat(20);
+//     alert(result);
+//   } else{
+//     alert("C");
+//   }
+
+
+
+      
+
+  
+
+//   switch (cityTypes) {
+//     case x :
+//       alert(B);
+//       break;
+//     case n :
+//       alert(A);
+//       break;
+//     case m,y :
+//       alert(C);
+//       break;
+//      default:
+//       alert("This is not a valid operation");
+//   }
+
+
+
+
+
+
+
+
+
+
+// let num = prompt("ENTER ANY NUMBER");
+
+// if (num % 3 ===0  && num % 5 ===0) {
+//   alert("FIZZBUZZ");
+// } else if (num % 3 === 0) {
+//   alert("FIZZ")
+// }
+// else if (num % 5 === 0) {
+//   alert("BUZZ")
+// }
+// else {
+//   alert("NUM IS NOT DIVISIBLE BY 3 AND 5");
+// };
+
+
+
+// let num = prompt("ENTER ANY NUMBER");
+
+// if (num % 3 ===0  && num % 5 ===0) 
+//   alert("FIZZBUZZ");
+//  else if (num % 3 === 0) 
+//   alert("FIZZ");
+
+// else if (num % 5 === 0) 
+//   alert("BUZZ");
+
+// else 
+//   alert("NUM IS NOT DIVISIBLE BY 3 AND 5");
+
+
+// syntax for loop
+
+
+// for(){
+
+// }
+
+
+// for (let i = 0; i < 12; i++) {
+//   let isDivisible = i % 2 ===0;
+//   let cat = i;
+// if (isDivisible) {
+//   alert("${i} cat is divisible by 2")
+// }
+//   else {
+//     alert("${i} is an odd nuimber")
+
+//   }
+  
+// }
+
+// for (let i = 0; i < 12; i++) {
+//   let isDivisible = i % 2 ===0;
+// if (isDivisible) {
+//   alert(`${i} is divisible by 2`)
+// }
+//   else {
+//     alert(`${i} is an odd nuimber`)
+
+//   }
+  
+// }
+
+
+
+
+// array 
+// array is an Object. object is group of item and operation that can be perform on them
+
+let array = [1,2,3,4,7,8,10,12];
+// array.push(9)
+// array.push(11)
+// array.push(13, 50)
+
+// alert(array);
+
+// array.length
+
+// alert(array.length);
+
+// alert(array[8])
+
+// array.push(Joshua)
+// alert(array);
+
+
+// for (let i = 0; i < array.length; i++) {
+//   alert(array[i]);
+  
+// }
+
+
+
+
+
+
+
+// const firstArr = [1,2,3,4,5,6,7,8,9, 10]
+// for(let i = 0; i, .length; i++){
+//   alert(p)
+//   alert(p.[0])
+// }
+
+// initialize an array with any number 1 to 10
+
+
+// const number = [1,2,3,4,5,6,7,8,9,10,11,12]
+// const even = []
+// const odd = []
+
+// for (let i=0;)
+
+
